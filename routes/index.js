dc9bdf1f8d28f601b0cd7954b0f55d2a4e94e293
engrowais <owaiskhan772@gmail.com>
2017-06-17 17:15:49 +0500

    added checkout files

--- routes/index.js
+++ routes/index.js
@@ -38,4 +38,12 @@ router.get('/shopping-cart', function(req, res, next){
   res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
+router.get('/checkout', function(req, res, next){
+  if(!req.session.cart){
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/checkout', {totalPrice: cart.totalPrice});
+});
+
 module.exports = router;
